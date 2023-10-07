1aa50024043ba580d6bd02ae726b527779bac015
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-10-08 00:46:40 +0300

    version1

---
+++ homework5/script5.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector("#btn");
+var out = document.querySelector("#out");
+var kolInput = document.getElementById("kol");
+var select1 = document.getElementById("select1");
+
+window.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+    btn.onclick = function calculatePrice() {
+        var quantity = parseInt(kolInput.value);  
+        if (isNaN(quantity) || quantity <= 0) {
+            alert("Пожалуйста, введите корректное количество товаров.");
+            kolInput.value = "";
+            select1.value = "";
+            return;
+        }
+        var productValue = parseInt(select1.value);
+        var price = quantity * productValue;  
+        out.innerHTML = "Цена: " + price + "₽";
+    }
+})

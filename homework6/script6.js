4971b4d2d3c7ee8f9b4e4d244247dc0269ccd036
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-10-16 18:46:56 +0300

    version4

--- homework6/script6.js
+++ homework6/script6.js
@@ -6,15 +6,17 @@ window.addEventListener("DOMContentLoaded", function () {
   const productSelect = document.getElementById("product");
   const selectElement = document.getElementById("select");
   const checkboxElement = document.getElementById("checkbox");
+  const checkbox = document.querySelector("input[name='checkbox']");
+  const radioButtons = document.querySelectorAll("input[type='radio']");
 
   const prices = {
+    checkbox1: 5,
     product1: 1500,
     product2: 0,
     product3: 1000,
     select1: 500,
     select2: 1000,
     select3: 2000,
-    checkbox1: 5,
   };
 
   let previousChecked = null;
@@ -23,10 +25,15 @@ window.addEventListener("DOMContentLoaded", function () {
     const quantity = parseInt(kolInput.value);
     const selectedProduct = productSelect.querySelector("input:checked").value;
 
-    const validQuantity = /^[1-9]\d*$/.test(quantity);
-    if (!validQuantity || quantity < 0) {
+    const validQuantity = /^[1-9][0-9]*$/.test(quantity);
+    if (!validQuantity) {
       alert("Пожалуйста, введите корректное количество услуг.");
       kolInput.value = "";
+      radioButtons.forEach(function(button) {
+        button.checked = false;
+      });
+      selectElement.style.display = "none";
+      checkboxElement.style.display = "none";
       out.innerHTML = "";
       return;
     }
@@ -46,8 +53,13 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     } else if (selectedProduct === "product3") {
       selectElement.style.display = "none";
-      checkboxElement.style.display = "block";      
-    }    
+      checkboxElement.style.display = "block";
+      const checkboxValue = checkbox.value;
+      const checkboxPrice = prices[checkboxValue];
+      if (checkbox.checked) {
+      totalCost = prices.product3 * checkboxPrice * quantity;
+      }
+    }
 
     out.innerHTML = "Цена: " + totalCost + "₽";
   }

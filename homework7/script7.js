582da4d824e338bd3214e1b779ce0616579e579b
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-10-22 13:23:14 +0300

    version2

--- homework7/script7.js
+++ homework7/script7.js
@@ -2,32 +2,66 @@ window.addEventListener("DOMContentLoaded", function () {
   console.log("DOM fully loaded and parsed");
 
   $(document).ready(function(){
-    $('.slider').slick({
+    $(".slider").slick({
+      adaptiveHeight: true,
       arrows: true,
       dots: true,
-      adaptiveHeight: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      speed: 1000,
-      easing: 'ease',
       draggable: false,
-      waitForAnimate: true,
+      easing: "ease",
       responsive: [
         {
           breakpoint: 760,
           settings: {
-            slidesToShow: 2,
             slidesToScroll: 2,
+            slidesToShow: 2
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            slidesToShow: 1
           }
         }
-      ]
+      ],
+      slidesToScroll: 3,
+      slidesToShow: 3,
+      speed: 1000,
+      waitForAnimate: true
     });
   });
 });
+
+// window.addEventListener("DOMContentLoaded", function () {
+//   console.log("DOM fully loaded and parsed");
+
+//   $(document).ready(function(){
+//     $('.slider').slick({
+//       arrows: true,
+//       dots: true,
+//       adaptiveHeight: true,
+//       slidesToShow: 3,
+//       slidesToScroll: 3,
+//       speed: 1000,
+//       easing: 'ease',
+//       draggable: false,
+//       waitForAnimate: true,
+//       responsive: [
+//         {
+//           breakpoint: 760,
+//           settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2,
+//           }
+//         },
+//         {
+//           breakpoint: 480,
+//           settings: {
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//           }
+//         }
+//       ]
+//     });
+//   });
+// });

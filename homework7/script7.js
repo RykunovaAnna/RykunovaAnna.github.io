2ad268f6bdb5165a3cd3b01124977c33b6ab7e63
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-10-23 12:10:56 +0300

    version2

--- homework7/script7.js
+++ homework7/script7.js
@@ -31,37 +31,3 @@ window.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// window.addEventListener("DOMContentLoaded", function () {
-//   console.log("DOM fully loaded and parsed");
-
-//   $(document).ready(function(){
-//     $('.slider').slick({
-//       arrows: true,
-//       dots: true,
-//       adaptiveHeight: true,
-//       slidesToShow: 3,
-//       slidesToScroll: 3,
-//       speed: 1000,
-//       easing: 'ease',
-//       draggable: false,
-//       waitForAnimate: true,
-//       responsive: [
-//         {
-//           breakpoint: 760,
-//           settings: {
-//             slidesToShow: 2,
-//             slidesToScroll: 2,
-//           }
-//         },
-//         {
-//           breakpoint: 480,
-//           settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1
-//           }
-//         }
-//       ]
-//     });
-//   });
-// });

b117fb70dc434add559a9c788e3713a5a550d91a
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-10-26 12:17:11 +0300

    version5
    validator

--- homework7/script7.js
+++ homework7/script7.js
@@ -1,33 +1,31 @@
-window.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM fully loaded and parsed");
+/*hslint browser: true*/ /*global $*/
 
-  $(document).ready(function(){
-    $(".slider").slick({
+$(document).ready(function () {
+  $(".slider").slick({
       adaptiveHeight: true,
       arrows: true,
       dots: true,
       draggable: false,
       easing: "ease",
       responsive: [
-        {
-          breakpoint: 760,
-          settings: {
-            slidesToScroll: 2,
-            slidesToShow: 2
+          {
+              breakpoint: 760,
+              settings: {
+                  slidesToScroll: 2,
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToScroll: 1,
+                  slidesToShow: 1
+              }
           }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToScroll: 1,
-            slidesToShow: 1
-          }
-        }
       ],
       slidesToScroll: 3,
       slidesToShow: 3,
       speed: 1000,
       waitForAnimate: true
-    });
   });
 });

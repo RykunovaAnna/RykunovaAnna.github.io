1586e6ddfadc36e571d7f0665b56a906ea4988d8
RykunovaAnna <144057821+RykunovaAnna@users.noreply.github.com>
2023-11-06 23:58:15 +0300

    version 3
    validator

--- homework8/script8.js
+++ homework8/script8.js
@@ -1,72 +1,5 @@
-// "use strict"
-// const $popOverlay = $(".popup-overlay");
-// const $popWindow = $(".popWindow");
-// const $popupMainWindow = $(".popup-main-window");
-// const $popThankYouWindow = $(".thank-you-window");
-// const $popClose = $(".close-btn");
-// const $popOpen = $("#for-popup-form");
-
-// $(function() {
-// $popClose.on("click", function() {
-// history.replaceState(null, null, ' ');
-// $popOverlay.fadeOut();
-// $popWindow.fadeOut();
-// $popThankYouWindow.fadeOut();
-// });
-
-// $(document).on("click", function(event) {
-// if ($(event.target).closest($popWindow).length) return;
-// history.replaceState(null, null, ' ');
-// $popOverlay.fadeOut();
-// $popWindow.fadeOut();
-// $popThankYouWindow.fadeOut();
-// event.stopPropagation();
-// });
-
-// $(".popup-form").submit(function() {
-//     var th = $(this);
-//     $(".popup-submit").prop('disabled', true);
-//     $.ajax({
-//     type: "POST",
-//     url: "https://formcarry.com/s/",
-//     data: th.serialize(),
-//     })
-//     $popupMainWindow.fadeOut();
-//     $popThankYouWindow.fadeIn();
-//     setTimeout(function() {
-//     $(".popup-submit").prop('disabled', false);
-//     th.trigger("reset");
-//     }, 1000);
-//     return false;
-//     });
-// });
-
-// $(document).ready(function() {
-// $popOpen.click(function(){
-// window.location.hash = "#popup";
-// $popOverlay.fadeIn();
-// $popupMainWindow.fadeIn();
-// return false;
-// });
-// });
-
-// $(window).on('hashchange', function (event) { 
-// if(window.location.hash == "#popup"){
-// $popOverlay.fadeIn();
-// $popupMainWindow.fadeIn();
-// }
-// else{
-// if(window.location.hash != "#popup") {
-// $popOverlay.fadeOut();
-// $popupMainWindow.fadeOut();
-// $popThankYouWindow.fadeOut();
-// }
-// }
-// });
-
-"use strict";
-
-$(function() {
+/*jslint browser: true*/ /*global $*/
+$(function () {
     const $popOverlay = $(".popup-overlay");
     const $popWindow = $(".popWindow");
     const $popupMainWindow = $(".popup-main-window");
@@ -74,31 +7,32 @@ $(function() {
     const $popClose = $(".close-btn");
     const $popOpen = $("#for-popup-form");
 
-    $popClose.on("click", function() {
-        history.replaceState(null, null, ' ');
+    $popClose.on("click", function () {
+        window.history.replaceState(null, null, " ");
         $popOverlay.fadeOut();
         $popWindow.fadeOut();
         $popThankYouWindow.fadeOut();
     });
 
-    $(document).on("click", function(event) {
-        if ($(event.target).closest($popWindow).length) return;
-        history.replaceState(null, null, ' ');
+    $(document).on("click", function (event) {
+        if ($(event.target).closest($popWindow).length) {
+            return;
+        }
+        window.history.replaceState(null, null, " ");
         $popOverlay.fadeOut();
         $popWindow.fadeOut();
         $popThankYouWindow.fadeOut();
         event.stopPropagation();
     });
 
-    $(".popup-form").submit(function() {
+    $(".popup-form").submit(function () {
         var th = $(this);
         var allFieldsFilled = true;
 
-        // Проверяем каждое обязательное поле
-        th.find("input, textarea").each(function() {
-            if ($(this).prop('required') && $(this).val() === "") {
+        th.find("input, textarea").each(function () {
+            if ($(this).prop("required") && $(this).val() === "") {
                 allFieldsFilled = false;
-                return false; // Прерываем цикл, если хотя бы одно поле не заполнено
+                return false;
             }
         });
 
@@ -107,37 +41,37 @@ $(function() {
             return false;
         }
 
-        $(".popup-submit").prop('disabled', true);
+        $(".popup-submit").prop("disabled", true);
         $.ajax({
-            type: "POST",
-            url: "https://formcarry.com/s/",
             data: th.serialize(),
+            type: "POST",
+            url: "https://formcarry.com/s/"
         });
 
         $popupMainWindow.fadeOut();
         $popThankYouWindow.fadeIn();
 
-        setTimeout(function() {
-            $(".popup-submit").prop('disabled', false);
+        setTimeout(function () {
+            $(".popup-submit").prop("disabled", false);
             th.trigger("reset");
         }, 1000);
 
         return false;
     });
 
-    $popOpen.click(function(){
+    $popOpen.click(function () {
         window.location.hash = "#popup";
         $popOverlay.fadeIn();
         $popupMainWindow.fadeIn();
         return false;
     });
 
-    $(window).on('hashchange', function (event) {
-        if(window.location.hash == "#popup"){
+    $(window).on("hashchange", function () {
+        if (window.location.hash === "#popup") {
             $popOverlay.fadeIn();
             $popupMainWindow.fadeIn();
         } else {
-            if(window.location.hash != "#popup") {
+            if (window.location.hash !== "#popup") {
                 $popOverlay.fadeOut();
                 $popupMainWindow.fadeOut();
                 $popThankYouWindow.fadeOut();
